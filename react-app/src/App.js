41144a7d1aba335b86f4869ba3efbeb45d5f6716
sedrainfo <taherhaouet@gmail.com>
2023-05-04 11:03:39 +0200

    Initial commit

---
+++ react-app/src/App.js
@@ -0,0 +1,26 @@
+
+import './App.css';
+import {Route,Routes} from 'react-router-dom';
+import Home from './components/pages/Home';
+import Products from './components/pages/Products';
+import AboutUs from './components/pages/AboutUs';
+import Contact from './components/pages/Contact';
+import Error from './components/pages/Error';
+
+
+
+function App() {
+  return (
+  
+      <Routes>
+                <Route exact path="/" element={<Home/>} />                
+                <Route path="/products" element={<Products />} />
+                <Route path="/about" element={<AboutUs />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="*" element={<Error />} />
+     </Routes>
+  
+  );
+}
+
+export default App;
